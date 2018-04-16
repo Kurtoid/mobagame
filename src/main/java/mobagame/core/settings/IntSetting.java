93b6c474a7a7f508682a189220d528685fc19dd5
Kurt Wilson <Kurt4wilson@gmail.com>
2018-04-16 17:18:15 -0400

    settings and .gitignore for windows

---
+++ src/main/java/mobagame/core/settings/IntSetting.java
@@ -0,0 +1,18 @@
+package mobagame.core.settings;
+
+import java.awt.Component;
+
+public class IntSetting extends Setting<Integer> {
+
+	public IntSetting(String name, int value) {
+		this.name = name;
+		this.value = value;
+	}
+
+	@Override
+	public Component getWidget() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
