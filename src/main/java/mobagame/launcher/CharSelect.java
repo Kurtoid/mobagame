cf9804ac59a339c3d890dcdc79e0b6a0399b5944
Katelynn Morrison <ktaces@gmail.com>
2018-05-03 08:08:32 -0400

    Fixed carson's problem with grid bag

--- src/main/java/mobagame/launcher/CharSelect.java
+++ src/main/java/mobagame/launcher/CharSelect.java
@@ -1,11 +1,12 @@
 package mobagame.launcher;
+
 //Carson Mango 4/25/18
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.*;
 
-public class CharSelect implements Runnable{
+public class CharSelect implements Runnable {
 	private JLabel timer = new JLabel("90");
 	JFrame selectionScreen = new JFrame("Character Select");
 	JPanel blueTeamSelect = new JPanel();
@@ -24,9 +25,10 @@ public class CharSelect implements Runnable{
 	JPanel red5 = new JPanel();
 	public ImageIcon placeHolderImage = new ImageIcon("E://ImpressMeProject/Black.png");
 	public ImageIcon reaperCharPic = new ImageIcon("E://ImpressMeProject/Reaper.png");
-	public void run(){
+
+	public void run() {
 		String temp = "90";
-		for(int i = 90; i >= 0; i--) {
+		for (int i = 90; i >= 0; i--) {
 			try {
 				Thread.sleep(1000);
 			} catch (InterruptedException e) {
@@ -36,13 +38,16 @@ public class CharSelect implements Runnable{
 			selectionScreen.setVisible(true);
 		}
 		selectionScreen.setVisible(false);
-		new GameScreen();
+		 new GameScreen();
 	}
+
 	public void start() {
 		Thread t = new Thread(this);
 		t.start();
 	}
+
 	private GridBagConstraints gbc = new GridBagConstraints();
+
 	public CharSelect() {
 		blueTeamSelect.setSize(750, 400);
 		redTeamSelect.setSize(750, 400);
@@ -100,16 +105,16 @@ public class CharSelect implements Runnable{
 		selectionScreen.setSize(1500, 800);
 		blueTeamSelect.setLayout(new GridLayout(5, 1, 5, 5));
 		redTeamSelect.setLayout(new GridLayout(5, 1, 5, 5));
-		blue1.setLayout(new GridLayout(1,2, 0, 0));
-		blue2.setLayout(new GridLayout(1,2, 0, 0));
-		blue3.setLayout(new GridLayout(1,2, 0, 0));
-		blue4.setLayout(new GridLayout(1,2, 0, 0));
-		blue5.setLayout(new GridLayout(1,2, 0, 0));
-		red1.setLayout(new GridLayout(1,2, 0, 0));
-		red2.setLayout(new GridLayout(1,2, 0, 0));
-		red3.setLayout(new GridLayout(1,2, 0, 0));
-		red4.setLayout(new GridLayout(1,2, 0, 0));
-		red5.setLayout(new GridLayout(1,2, 0, 0));
+		blue1.setLayout(new GridLayout(1, 2, 0, 0));
+		blue2.setLayout(new GridLayout(1, 2, 0, 0));
+		blue3.setLayout(new GridLayout(1, 2, 0, 0));
+		blue4.setLayout(new GridLayout(1, 2, 0, 0));
+		blue5.setLayout(new GridLayout(1, 2, 0, 0));
+		red1.setLayout(new GridLayout(1, 2, 0, 0));
+		red2.setLayout(new GridLayout(1, 2, 0, 0));
+		red3.setLayout(new GridLayout(1, 2, 0, 0));
+		red4.setLayout(new GridLayout(1, 2, 0, 0));
+		red5.setLayout(new GridLayout(1, 2, 0, 0));
 		blue1.add(blue1CharImage);
 		blue2.add(blue2CharImage);
 		blue3.add(blue3CharImage);
@@ -161,7 +166,9 @@ public class CharSelect implements Runnable{
 		gbc.gridy = 3;
 		gbc.gridx = 0;
 		gbc.weightx = 1;
-		selectionScreen.add(charStats);
+		// Found your problem Carson, you were not using the constraint. KEM
+		// selectionScreen.add(charStats); old line
+		selectionScreen.add(charStats, gbc); // new line
 		selectionScreen.setVisible(true);
 		start();
 		System.out.println(gbc.gridy);
