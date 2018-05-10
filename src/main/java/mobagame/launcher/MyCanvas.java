433863b6aadcd4c26a5b6f25e7e6ae2253ee4c37
Kurt Wilson <kurt4wilson@gmail.com>
2018-05-10 07:51:32 -0400

    fixed gamescreen item rendering
    Signed-off-by: Kurt Wilson <kurt4wilson@gmail.com>

--- src/main/java/mobagame/launcher/MyCanvas.java
+++ src/main/java/mobagame/launcher/MyCanvas.java
@@ -1,41 +1,52 @@
 package mobagame.launcher;
 
+import java.awt.Dimension;
 import java.awt.Graphics;
-import java.awt.Image;
-import java.awt.Toolkit;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
+import javax.imageio.ImageIO;
 import javax.swing.JComponent;
 import javax.swing.JFrame;
 
 @SuppressWarnings("serial")
 public class MyCanvas extends JComponent {
-	
+
 	String location;
 	int scale;
-	
-	public MyCanvas(String location, int scale){
+
+	public MyCanvas(String location, int scale) {
+		super();
 		this.location = location;
 		this.scale = scale;
+		this.setPreferredSize(new Dimension(scale,  scale));
 	}
-	
-  public void paint(Graphics g) {
-    Image img1 = Toolkit.getDefaultToolkit().getImage(this.location);
 
-    int width = img1.getWidth(this);
-    int height = img1.getHeight(this);
+	public void paint(Graphics g) {
+		BufferedImage img1 = null;
+		try {
+			img1 = ImageIO.read(new File(location));
 
-    int w = scale * width;
-    int h = scale * height;
-    // explicitly specify width (w) and height (h)
-    g.drawImage(img1, 0, 0, (int) w, (int) h, this);
+			int width = img1.getWidth(this);
+			int height = img1.getHeight(this);
 
-  }
-  
-  public static void main(String[] a) {
-	    JFrame window = new JFrame();
-	    window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	    window.setBounds(30, 30, 300, 300);
-	    window.getContentPane().add(new MyCanvas("resources/black.png", 1));
-	    window.setVisible(true);
-	  }
+			int w = scale /* width */;
+			int h = scale /* height*/;
+			// explicitly specify width (w) and height (h)
+			g.drawImage(img1, 0, 0, (int) w, (int) h, this);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+	public static void main(String[] a) {
+		JFrame window = new JFrame();
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		window.setBounds(30, 30, 300, 300);
+		window.getContentPane().add(new MyCanvas("resources/black.png", 100));
+		window.setVisible(true);
+	}
 }
