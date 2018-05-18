0ff2232ead8db52d950c9a0a45a3f4905ad4a87c
WeedMaster27 <mangocarson@gmail.com>
2018-05-18 09:34:48 -0400

    fixing up character ability and ingame player but still need to finish and fix charselect issue

--- src/main/java/mobagame/core/game/Ability.java
+++ src/main/java/mobagame/core/game/Ability.java
@@ -6,7 +6,79 @@ private int manaCost;
 private int baseDamage;
 private int abiPowRatio;
 private int phyPowRatio;
+private int damScale;
+public int getDamScale() {
+	return damScale;
+}
+public void setDamScale(int damScale) {
+	this.damScale = damScale;
+}
+public InGamePlayer getCaster() {
+	return caster;
+}
+public void setCaster(InGamePlayer caster) {
+	this.caster = caster;
+}
 //private AbilityType abiType;
 private int range;
 private String imageLocation;
+private String damageType;
+private InGamePlayer caster;
+public String getAbiName() {
+	return abiName;
+}
+public void setAbiName(String abiName) {
+	this.abiName = abiName;
+}
+public int getManaCost() {
+	return manaCost;
+}
+public void setManaCost(int manaCost) {
+	this.manaCost = manaCost;
+}
+public int getBaseDamage() {
+	return baseDamage;
+}
+public void setBaseDamage(int baseDamage) {
+	this.baseDamage = baseDamage;
+}
+public int getAbiPowRatio() {
+	return abiPowRatio;
+}
+public void setAbiPowRatio(int abiPowRatio) {
+	this.abiPowRatio = abiPowRatio;
+}
+public int getPhyPowRatio() {
+	return phyPowRatio;
+}
+public void setPhyPowRatio(int phyPowRatio) {
+	this.phyPowRatio = phyPowRatio;
+}
+public int getRange() {
+	return range;
+}
+public void setRange(int range) {
+	this.range = range;
+}
+public String getImageLocation() {
+	return imageLocation;
+}
+public void setImageLocation(String imageLocation) {
+	this.imageLocation = imageLocation;
+}
+public String getDamageType() {
+	return damageType;
+}
+public void setDamageType(String damageType) {
+	this.damageType = damageType;
+}
+public int getDamage() {
+	int damage;
+	if(this.damageType == "Physical") {
+		damage = caster.getPhyPow() * this.phyPowRatio + this.baseDamage;
+	}else{
+		damage = caster.getAbiPow() * this.abiPowRatio + this.baseDamage;
+	}
+	return damage;
+}
 }
--- src/main/java/mobagame/core/game/Character.java
+++ src/main/java/mobagame/core/game/Character.java
@@ -11,11 +11,110 @@ public class Character {
 	private Ability[] abilitys = new Ability[5];
 
 	private int range; // Measured in custom units ru
-	private int phyPow;
-	private int abiPow;
+	private int basePhyPow;
+	private int baseAbiPow;
 	private int phyPowScale;
 	private int abiPowScale;
 	private int speed;
+	private int baseArmor;
+	private int armorScale;
+	private int baseMagicResist;
+	private int magicResistScale;
+	private int maxHealthScale;
+	private int maxManaScale;
+	//Need to fix these
+	private Ability abiq;
+	private Ability abiw;
+	private Ability abie;
+	private Ability abir;
+
+	public int getMaxHealthScale() {
+		return maxHealthScale;
+	}
+
+	public void setMaxHealthScale(int maxHealthScale) {
+		this.maxHealthScale = maxHealthScale;
+	}
+
+	public int getMaxManaScale() {
+		return maxManaScale;
+	}
+
+	public void setMaxManaScale(int maxManaScale) {
+		this.maxManaScale = maxManaScale;
+	}
+
+	public int getBasePhyPow() {
+		return basePhyPow;
+	}
+
+	public void setBasePhyPow(int basePhyPow) {
+		this.basePhyPow = basePhyPow;
+	}
+
+	public int getBaseAbiPow() {
+		return baseAbiPow;
+	}
+
+	public void setBaseAbiPow(int baseAbiPow) {
+		this.baseAbiPow = baseAbiPow;
+	}
+
+	public int getBaseArmor() {
+		return baseArmor;
+	}
+
+	public void setBaseArmor(int baseArmor) {
+		this.baseArmor = baseArmor;
+	}
+
+	public int getArmorScale() {
+		return armorScale;
+	}
+
+	public void setArmorScale(int armorScale) {
+		this.armorScale = armorScale;
+	}
+
+	public int getBaseMagicResist() {
+		return baseMagicResist;
+	}
+
+	public void setBaseMagicResist(int baseMagicResist) {
+		this.baseMagicResist = baseMagicResist;
+	}
+
+	public int getMagicResistScale() {
+		return magicResistScale;
+	}
+
+	public void setMagicResistScale(int magicResistScale) {
+		this.magicResistScale = magicResistScale;
+	}
+
+	public void setCharName(String charName) {
+		this.charName = charName;
+	}
+
+	public void setAbilitys(Ability[] abilitys) {
+		this.abilitys = abilitys;
+	}
+
+	public void setRange(int range) {
+		this.range = range;
+	}
+
+	public void setPhyPowScale(int phyPowScale) {
+		this.phyPowScale = phyPowScale;
+	}
+
+	public void setAbiPowScale(int abiPowScale) {
+		this.abiPowScale = abiPowScale;
+	}
+
+	public void setImageLocation(String imageLocation) {
+		this.imageLocation = imageLocation;
+	}
 
 	private String imageLocation;
 
@@ -62,11 +161,11 @@ public class Character {
 	}
 
 	public int getPhyPow() {
-		return phyPow;
+		return basePhyPow;
 	}
 
 	public int getAbiPow() {
-		return abiPow;
+		return  baseAbiPow;
 	}
 
 	public int getPhyPowScale() {
