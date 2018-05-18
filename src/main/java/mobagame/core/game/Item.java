269be8b264f07d40273de968a4e601957d7d4761
Katelynn Morrison <ktaces@gmail.com>
2018-05-18 08:35:54 -0400

    Worked on items

--- src/main/java/mobagame/core/game/Item.java
+++ src/main/java/mobagame/core/game/Item.java
@@ -6,14 +6,15 @@ public class Item {
 	private String name;
 	private String imageLocation;
 	private int price;
-	private int effectPoints;
+	private int[] effectPoints;
 	private boolean isConsumable;
-	// private ItemType type;
+	private ItemType[] type;
 
-	public Item(String name, String imageLocation, int price, int effectPoints, boolean isConsumable) {
+	public Item(String name, String imageLocation, int price,ItemType[] type, int[] effectPoints, boolean isConsumable) {
 		this.name = name;
 		this.imageLocation = imageLocation;
 		this.price = price;
+		this.type = type;
 		this.effectPoints = effectPoints;
 		this.isConsumable = isConsumable;
 	}
@@ -26,6 +27,34 @@ public class Item {
 					if (gold >= price) {
 						gold -= price;
 						user.inventory[y][x] = this;
+						if (!isConsumable) {
+							for (int z = 0; z < type.length; z++) {
+								switch (type[z]) {
+								case Health:
+//									user.setMaxHealth(user.getMaxHealth() + effectPoints[z]);
+									break;
+								case Mana:
+									break;
+								case PhysicalPower:
+									break;
+								case AbilityPower:
+									break;
+								case Speed:
+									break;
+								case Attack:
+									break;
+								case AttackSpecial:
+									break;
+								case Armor:
+									break;
+								case MagicResistance:
+									break;
+								default:
+									System.out.println("ERROR: Unknown item type");
+									break;
+								}
+							}
+						}
 						return;
 					} else {
 						System.out.println("Not enought gold to buy " + this.name);
---
+++ src/main/java/mobagame/core/game/ItemType.java
@@ -0,0 +1,13 @@
+package mobagame.core.game;
+
+public enum ItemType {
+	Health,
+	Mana,
+	PhysicalPower,
+	AbilityPower,
+	Speed,
+	Attack,
+	AttackSpecial,
+	Armor,
+	MagicResistance
+}
