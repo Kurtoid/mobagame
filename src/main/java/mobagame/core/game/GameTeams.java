e156d36aa9a2ce5c0c2557d160a7ea14e87e5812
Kurt Wilson <kurt4wilson@gmail.com>
2018-05-26 12:18:27 -0400

    Team support

---
+++ src/main/java/mobagame/core/game/GameTeams.java
@@ -0,0 +1,8 @@
+package mobagame.core.game;
+
+import mobagame.core.game.Team;
+
+public class GameTeams {
+	static Team highTeam = new Team("Red");
+	static Team lowTeam = new Team("Blue");
+}
---
+++ src/main/java/mobagame/core/game/Team.java
@@ -0,0 +1,9 @@
+package mobagame.core.game;
+
+public class Team {
+	String name;
+
+	public Team(String name) {
+		this.name = name;
+	}
+}
