5fa8f47faefb9657fe143045e7e0b01841d43e2d
WeedMaster27 <mangocarson@gmail.com>
2018-05-28 16:50:27 -0400

    Jack and temp character select stuff

--- src/main/java/mobagame/core/game/GameCharcters.java
+++ src/main/java/mobagame/core/game/GameCharcters.java
@@ -5,9 +5,9 @@ import java.util.List;
 
 public class GameCharcters {
 
-	public final static Character reaper = new Character("reaper", "resources/Character/reaper.pmg", 1, 50, 20, 500, 20, 375,
+	public final static Character reaper = new Character("reaper", "resources/Character/Reaper.png", 1, 50, 20, 500, 20, 375,
 			5, 25, 5, 25, 2, 10, 2, 10);//Still need to be balanced
-	public final static Character jack = new Character("Jack", "", 150, 50, 20, 575, 25, 370, 7, 64, 0, 0, 5, 5, 7, 7);
+	public final static Character jack = new Character("Jack", "resources/Character/Jack.png", 150, 50, 20, 575, 25, 370, 7, 64, 0, 0, 5, 5, 7, 7);
 	public static final Character[] allGameCharacter = {reaper, jack};
 	public static final List<Character> allGameCharacterLookup = Arrays.asList(allGameCharacter);
 }
