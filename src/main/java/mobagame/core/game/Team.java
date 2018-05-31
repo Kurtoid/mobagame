a9f0298bad7a31c043e22b35fb1a380c9ec3f815
Kurt Wilson <Kurt4wilson@gmail.com>
2018-05-30 22:06:49 -0400

    spawn points

--- src/main/java/mobagame/core/game/Team.java
+++ src/main/java/mobagame/core/game/Team.java
@@ -1,12 +1,15 @@
 package mobagame.core.game;
 
 import java.awt.*;
+import java.awt.geom.Point2D;
 
 public class Team {
 	String name;
 	public Color color;
-	public Team(String name, Color c) {
+	public Team(String name, Color c, Point2D.Double spawn) {
 		this.name = name;
 		this.color = c;
 	}
+
+	Point2D.Double spawnPoint;
 }
